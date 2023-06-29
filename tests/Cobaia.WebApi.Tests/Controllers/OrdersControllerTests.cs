7d263d82cf1b5fdd152743eb1ef1fb1fdebd764a
Gabriel Henrique da Silva Ferreira <ghenrique_10@hotmail.com>
2023-06-29 14:37:21 -0300

    Test for submit order endpoint.

---
+++ tests/Cobaia.WebApi.Tests/Controllers/OrdersControllerTests.cs
@@ -0,0 +1,43 @@
+﻿using Cobaia.WebApi.Commands.SubmitOrder;
+using Cobaia.WebApi.Controllers;
+using Cobaia.WebApi.Models;
+using Cobaia.WebApi.Tests.Utils;
+using FluentAssertions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using System;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Cobaia.WebApi.Tests.Controllers
+{
+    public class OrdersControllerTests
+    {
+        [Fact]
+        public async Task SubmitOrder_OrderCreated_ReturnsCreatedEntity()
+        {
+            // Arrange
+            var sender = Substitute.For<ISender>();
+            var request = new SubmitOrderCommand { OrderId = Guid.NewGuid() };
+            var createdEntity = new CreatedEntity(request.OrderId, DateTime.UtcNow);
+            sender.Send(request).Returns(createdEntity);
+            var controller = new OrdersController(
+                sender,
+                Substitute.For<ILogger<OrdersController>>(),
+                CobaiaWebApiContextFactory.CreateInMemory());
+
+            // Act
+            var response = await controller.SubmitOrder(request);
+
+            // Assert
+            response
+                .Should()
+                .BeOfType<OkObjectResult>();
+            (response as OkObjectResult)!.Value
+                .Should()
+                .Be(createdEntity);
+        }
+    }
+}
---
+++ tests/Cobaia.WebApi.Tests/Utils/CobaiaWebApiContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Cobaia.WebApi.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Cobaia.WebApi.Tests.Utils
+{
+    internal static class CobaiaWebApiContextFactory
+    {
+        public static CobaiaWebApiContext CreateInMemory()
+        {
+            var options = new DbContextOptionsBuilder<CobaiaWebApiContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new CobaiaWebApiContext(options);
+            context.Database.EnsureCreated();
+
+            return context;
+        }
+    }
+}
